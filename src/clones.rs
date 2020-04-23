dee33c559fb788960da28b255a5711f3622e64ff
merl <merl.001.mia@gmail.com>
2020-04-23 15:46:18 +0300

    up

--- src/clones.rs
+++ src/clones.rs
@@ -2,30 +2,31 @@
 use crate::{Flags,Filled};
 
 #[derive(Debug)]
-struct Slot<Key>{
+struct Slot<K,V>{
     flags: u64,
-    data: Vec<Key>,
+    data: Vec<(K,V)>,
 }
-impl<Key> Slot<Key> {
-    fn size() -> usize {
+impl<K,V> Slot<K,V> {
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+    fn max_size() -> usize {
         64
     }
 }
-impl<Key> Slot<Key>
-where Key: Ord
-{
-    fn new() -> Slot<Key> {
+impl<K: Ord,V> Slot<K,V> {
+    fn new() -> Slot<K,V> {
         Slot {
             flags: 0,
             data: Vec::with_capacity(64),
         }
     }
-    fn contains(&self, k: &Key) -> (Option<usize>,Option<usize>) { // Key slot idx + Empty slot idx if known
+    fn contains(&self, k: &K) -> (Option<usize>,Option<usize>) { // Key slot idx + Empty slot idx if known
         let mut fl = self.flags;
         let mut empty = None;
-        for (i,v) in self.data.iter().enumerate() {
+        for (i,(ki,_)) in self.data.iter().enumerate() {
             if (fl & 0x1) > 0 {
-                if v == k {
+                if ki == k {
                     return (Some(i),empty);
                 }
             } else {
@@ -35,23 +36,35 @@ where Key: Ord
         }
         (None,empty)
     }
-    fn insert(&mut self, k: Key) -> (bool,Filled) {
+    fn get(&mut self, k: &K) -> Option<&V> {
+        match self.contains(k).0 {
+            Some(idx) => Some(&self.data[idx].1),
+            None => None,
+        }
+    }
+    fn get_mut(&mut self, k: &K) -> Option<&mut V> {
+        match self.contains(k).0 {
+            Some(idx) => Some(&mut self.data[idx].1),
+            None => None,
+        }
+    }
+    fn insert(&mut self, k: K, v: V) -> (bool,Filled) {
         let idx = match self.contains(&k) {
             (Some(_),_) => return (true,if self.data.len() >= 64 { Filled::Full } else { Filled::HasSlots }),
             (None,None) => {
                 let idx = self.data.len();
-                self.data.push(k);
+                self.data.push((k,v));
                 idx
             },
             (None,Some(idx)) => {
-                self.data[idx] = k;
+                self.data[idx] = (k,v);
                 idx
             }
         };
         self.flags |= 0x1u64 << idx;
         (false,if self.data.len() >= 64 { Filled::Full } else { Filled::HasSlots })
     }
-    fn remove(&mut self, k: &Key) -> bool {
+    fn remove(&mut self, k: &K) -> bool {
         match self.contains(&k) {
             (Some(idx),_) => {
                 let msk = 0xFFFFFFFFFFFFFFFF - (0x1u64 << idx);
@@ -66,45 +79,43 @@ where Key: Ord
         self.flags = 0;
         self.data.clear();
     }
-    fn into_multislot(&mut self) -> MultiSlot<Key> {
-        let mut v = Vec::with_capacity(self.flags.count_ones() as usize);
+    fn into_multislot(&mut self) -> MultiSlot<K> {
+        let mut vc = Vec::with_capacity(self.flags.count_ones() as usize);
         let mut fl = self.flags;
-        for k in self.data.drain(..) {
+        for (k,v) in self.data.drain(..) {
             if (fl & 0x1) > 0 {
-                v.push(k);
+                vc.push(k);
             }
             fl >>= 1;
         }
-        v.sort();
+        vc.sort();
         self.clear();
         MultiSlot {
             _sz: 1,
             empty: false,
-            flags: Flags::ones(v.len()),
-            data: v,
+            flags: Flags::ones(vc.len()),
+            data: vc,
         }
     }
 }
 
 
-struct MultiSlot<Key> {
+struct MultiSlot<K> {
     _sz: usize,
     empty: bool,
     flags: Flags,
-    data: Vec<Key>,
+    data: Vec<K>,
 }
-impl<Key> MultiSlot<Key>
-    where Key: Ord
-{
-    fn empty(sz: usize) -> MultiSlot<Key> {   
+impl<K: Ord> MultiSlot<K> {
+    fn empty(sz: usize) -> MultiSlot<K> {   
         MultiSlot {
             _sz: sz,
             empty: true,
-            flags: Flags::nulls(Slot::<Key>::size() * (0x1 << (sz-1))),
+            flags: Flags::nulls(Slot::<K,()>::max_size() * (0x1 << (sz-1))),
             data: Vec::new(),
         }
-    }
-    fn contains(&self, k: &Key) -> Option<usize> {
+    }        
+    fn contains(&self, k: &K) -> Option<usize> {
         if (self.data.len() == 0)||(*k < self.data[0])||(*k > self.data[self.data.len()-1]) { return None; }
         match self.data.binary_search(k) {
             Ok(idx) => match self.flags.get(idx) {
@@ -113,27 +124,28 @@ impl<Key> MultiSlot<Key>
             },
             Err(_) => None,
         }
-    }
+    }            
     fn clear(&mut self) {
         self.empty = true;
         self.flags.set_nulls();
         self.data.clear();
     }
 }
+        
 
-
-pub struct CivSet<Key> {
+pub struct CivSet<K> {
     len: usize,
     _tombs: usize,
-    slot: Slot<Key>,
-    data: Vec<MultiSlot<Key>>,
+    slot: Slot<K,()>,
+    data: Vec<MultiSlot<K>>,
 
     tmp_c: usize,
-    tmp_merge_vec: Vec<Key>,
+    tmp_merge_vec: Vec<K>,
     tmp_merge_flags: Flags,
 }
-impl<Key: Ord> CivSet<Key> {
-    pub fn new() -> CivSet<Key> {
+       
+impl<K: Ord> CivSet<K> {
+    pub fn new() -> CivSet<K> {
         CivSet {
             len: 0,
             _tombs: 0,
@@ -145,13 +157,13 @@ impl<Key: Ord> CivSet<Key> {
             tmp_merge_flags: Flags::tmp(),
         }
     }
-    pub fn contains(&mut self, k: &Key) -> bool {
+    pub fn contains(&mut self, k: &K) -> bool {
         match self.slot.contains(k) {
             (Some(_),_) => true,
             (None,_) => self.multy_contains(k).is_some(),
         }
     }
-    fn multy_contains(&mut self, k: &Key) -> Option<(usize,usize)> {
+    fn multy_contains(&mut self, k: &K) -> Option<(usize,usize)> {
         for (n,ms) in self.data.iter().enumerate() {
             if let Some(idx) = ms.contains(k) {
                 return Some((n,idx));
@@ -159,11 +171,11 @@ impl<Key: Ord> CivSet<Key> {
         }
         None
     }
-    pub fn insert(&mut self, k: Key) -> bool {
+    pub fn insert(&mut self, k: K) -> bool {
         if self.multy_contains(&k).is_some() {
             return true;
         }
-        let (r,filled) = self.slot.insert(k);
+        let (r,filled) = self.slot.insert(k,());
         if let Filled::Full = filled {
             if self.data.len() == 0 {
                 self.data.push(self.slot.into_multislot());
@@ -184,7 +196,7 @@ impl<Key: Ord> CivSet<Key> {
     pub fn len(&self) -> usize {
         self.len
     }
-    pub fn remove(&mut self, k: &Key) -> bool {
+    pub fn remove(&mut self, k: &K) -> bool {
         match self.multy_contains(&k) {
             Some((msi,idx)) => {
                 self.data[msi].flags.unset(idx);
@@ -197,7 +209,7 @@ impl<Key: Ord> CivSet<Key> {
     }
     fn merge_into(&mut self, n: usize) -> Result<(),&'static str> {
         if !self.data[n].empty { return Err("data[n] is not empty"); }
-        let mut cnt = Slot::<Key>::size();
+        let mut cnt = self.slot.len();
         for i in 0 .. n {
             if self.data[i].empty { return Err("one of data[0..n] is empty"); }
             cnt += self.data[i].data.len();
@@ -206,27 +218,12 @@ impl<Key: Ord> CivSet<Key> {
         
         {
             let sl_flags = Flags(vec![self.slot.flags]);
-            for (i,k) in self.slot.data.drain(..).enumerate() {
+            for (i,(k,_)) in self.slot.data.drain(..).enumerate() {
                 if sl_flags.get(i) {
                     self.data[n].data.push(k);
                 }
             }
             self.slot.clear();
-
-            /*let mut tmp = Vec::new();
-            let mut sl_flags = Flags::tmp();
-            std::mem::swap(&mut self.data[n].data,&mut tmp);
-            for i in 0 .. n {
-                std::mem::swap(&mut self.data[i].flags,&mut sl_flags);
-                for (j,k) in self.data[i].data.drain(..).enumerate() {
-                    if sl_flags.get(j) {
-                        tmp.push(k);
-                    }
-                }
-                std::mem::swap(&mut self.data[i].flags,&mut sl_flags);
-                self.data[i].clear();
-            }
-            std::mem::swap(&mut self.data[n].data,&mut tmp);*/
 
             std::mem::swap(&mut self.data[n].data, &mut self.tmp_merge_vec);
             for i in 0 .. n {
@@ -252,3 +249,5 @@ impl<Key: Ord> CivSet<Key> {
         Ok(())
     }
 }
+
+        
